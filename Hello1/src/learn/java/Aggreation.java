156a1bf252993688325328551dc39c3ae46efaec
prasadk12 <prasadk0520@gmail.com>
2018-02-24 12:22:49 +0530

    Renamed the file name

--- Hello1/src/learn/java/Aggreation.java
+++ Hello1/src/learn/java/Aggreation.java
@@ -1,50 +1,53 @@
 package learn.java;
+/**
+ * Aggregation is a special form of association. 
+ * It is a relationship between two classes like association, however its a directional association, which means it is strictly a one way association. 
+ * It represents a HAS-A relationship.
+ * @author prasadk12
+ *
+ */
+class Address {
+
+	String city,state,country;  
+
+	public Address(String city, String state, String country) {  
+		this.city = city;  
+		this.state = state;  
+		this.country = country; 
+
+	}  
+
+}  
 
 
 
+class Emp {
 
-	     class Address22 {
-	    	
-	    String city,state,country;  
-	      
-	    public Address22(String city, String state, String country) {  
-	        this.city = city;  
-	        this.state = state;  
-	        this.country = country; 
-	        
-	    }  
-	      
-	    }  
+	int id;  
+	String name;  
+	Address address;  
 
+	public Emp(int id, String name,Address address) {  
+		this.id = id;  
+		this.name = name;  
+		this.address=address;  
+	}  
 
+	void display(){  
+		System.out.println(id+" "+name);  
+		System.out.println(address.city+" "+address.state+" "+address.country);  
+	}  
+}
+class Aggreation{
+	public static void main(String[] args) {  
+		Address address1=new Address("gzb","UP","india");  
+		Address address2=new Address("gno","UP","india");  
 
-	     class Emp22 {
-	    	
-	    int id;  
-	    String name;  
-	    Address22 address;  
-	      
-	    public Emp22(int id, String name,Address22 address) {  
-	        this.id = id;  
-	        this.name = name;  
-	        this.address=address;  
-	    }  
-	      
-	    void display(){  
-	    System.out.println(id+" "+name);  
-	    System.out.println(address.city+" "+address.state+" "+address.country);  
-	    }  
-	     }
-	     class Aggreation2{
-	    public static void main(String[] args) {  
-	    Address22 address1=new Address22("gzb","UP","india");  
-	    Address22 address2=new Address22("gno","UP","india");  
-	      
-	    Emp22 e=new Emp22(111,"varun",address1);  
-	    Emp22 e2=new Emp22(112,"arun",address2);  
-	          
-	    e.display();  
-	    e2.display();  
-	          
-	    }  
-	    }  
+		Emp e=new Emp(111,"varun",address1);  
+		Emp e2=new Emp(112,"arun",address2);  
+
+		e.display();  
+		e2.display();  
+
+	}  
+}  
